2513138478ebb2856bd38e7413965039d9f53311
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-15 17:09:41 +0100

    les autres composants

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import './App.css';
 
+// Le composant Pht
 const Pht = ()=> {
   return (
     <div className='form_control'>
       <label htmlFor='pht'>Prix Hors taxe : </label>
-      <input type='number' id='pht' />
+      <input type='number' id='pht' placeholder='pht....'/>
     </div>
+  );
+}
+
+// Le composant TVA
+const TVA = ()=> {
+  return (
+    <div className='form_control'>
+      <label htmlFor='tva'>Taux TVA : </label>
+      <input type='number' id='tva' placeholder='tva....'/>
+    </div>
+  );
+}
+
+// Le composants buttons
+const Buttons = ()=> {
+  return (
+    <div className='form_button'>
+      <button>Calculer</button>
+      <button>Initialiser</button>
+    </div>
+  );
+}
+
+// Le composant Resultat
+const Res = ()=> {
+  return (
+    <div className='form_control'>
+      <label htmlFor='res'>resultat : </label>
+      <input type='number' id='res' readOnly/>
+    </div>
+  );
+}
+
+const CalculPTTC = ()=> {
+  return (
+      <fieldset>
+        <legend>Calcul de PTTC</legend>
+        <Pht />
+        <TVA />
+        <Buttons />
+        <Res />
+      </fieldset>
   );
 }
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <CalculPTTC />
   );
 }
 
